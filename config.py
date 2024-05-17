775bdfe41aa03c5104267affc4036510ea77d09a
Maksim <maxikpetlitskii@gmail.com>
2024-05-17 18:05:25 +0200

    Adding all subscriptions for user

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 # Configuration settings
 
 # Telegram bot token
-TELEGRAM_BOT_TOKEN = "EXAMPLE"
+TELEGRAM_BOT_TOKEN = "EXMAPLE"
 
 # Directory to save images
 IMAGES_DIR = 'images/'
+
+# config.py
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
+
